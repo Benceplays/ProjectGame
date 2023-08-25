b43baae1ee8fa0607f6938928a9bec9af9e04300
Script Devil <71324001+Benceplays@users.noreply.github.com>
2023-08-25 23:58:12 +0200

    par cucc

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -1,27 +1,72 @@
 using Godot;
+using MySql.Data.MySqlClient;
 using System;
+using System.Reflection.PortableExecutable;
 
 public partial class Menu : Node2D
 {
     //valtozok
-    double time = 50;
+    static string dbserver = "127.0.0.1";
+    static string database = "projectgame";
+    static string dbusername = "user";
+    static string dbpassword = "joWAyE";
+    static MySqlConnection conn;
+    static MySqlDataReader reader;
+    bool SearchIsOn = false;
+    double time = 0;
     double sec = 0;
     double min = 0;
     string second;
     string minute;
-    //nodeok
+    //nodeok valtozoi
     Label timeLabel;
     public override void _Ready()
     {
         timeLabel = GetNode("PlayButton/TimeLabel") as Label;
+        new Machmaking();
     }
     public override void _Process(double delta)
     {
-        time += delta * 1.0;
-        if (time/59 == 1) { min++; time = 0; GD.Print("most"); }
-        else { sec = time; }
-        if (sec < 10) { second = $"0{(int)sec}"; } else { second = $"{(int)sec}"; }
-        if (min < 10) { minute = $"0{(int)min}"; } else { minute = $"{(int)min}"; }
-        timeLabel.Text = $"{minute}:{second}";
+        if (SearchIsOn)
+        {
+            time += delta * 1.0;
+            if (time >= 60) { min++; time = 0; } else { sec = time; }
+            if (sec < 10) { second = $"0{(int)sec}"; } else { second = $"{(int)sec}"; }
+            if (min < 10) { minute = $"0{(int)min}"; } else { minute = $"{(int)min}"; }
+            timeLabel.Text = $"{minute}:{second}";
+        }
+    }
+    public void ServerSearchButton() { SearchIsOn = true; }
+}
+public class Machmaking
+{
+    static string dbserver = "127.0.0.1";
+    static string database = "projectgame";
+    static string dbusername = "user";
+    static string dbpassword = "joWAyE";
+    static MySqlConnection conn;
+    static MySqlDataReader reader;
+    static Machmaking()
+    {
+        GD.Print("asd");
+        Connection();
+    }
+    public static void Connection()
+    {
+        string constring = $"SERVER={dbserver};DATABASE={database};UID={dbusername};PASSWORD={dbpassword};";
+        Console.WriteLine("Elindult");
+        conn = new MySqlConnection(constring);
+        conn.Open();
+    }
+    public static void ServerSearch()
+    {
+        Connection();
+        string query = $"SELECT * FROM servers";
+        MySqlCommand cmd = new MySqlCommand(query, conn);
+        reader = cmd.ExecuteReader();
+        reader.Read();
+        Console.WriteLine("IP: " + reader["ip"] + " Current players: " + reader["currentplayers"] + " Max player: " + reader["maxplayer"]);
+        reader.Close();
+        Console.ReadKey();
     }
 }
